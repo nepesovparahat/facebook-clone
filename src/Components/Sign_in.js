626e96a6752dfe06bbecacc0114ea8514718a8e9
parahat <parahatnepesov1993.18.04@gmail.com>
2021-02-05 22:50:42 +0300

    Added a facebook logo image to the login page

--- src/Components/Sign_in.js
+++ src/Components/Sign_in.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
+import Logo from './logo.svg';
 import { Link } from 'react-router-dom';
 import { useHistory } from "react-router-dom";
 import { useForm } from 'react-hook-form';
@@ -14,7 +15,7 @@ const signInSchema = yup.object().shape({
         .required()
 })
 export const Sign_in = () => {
-    const { register, handleSubmit } = useForm({
+    const { register, handleSubmit} = useForm({
         mode: 'onSubmit',
         resolver: yupResolver(signInSchema),
     })
@@ -23,25 +24,28 @@ export const Sign_in = () => {
 
     const onSubmit = (data) => {
         if (data.email === admin.email) {
-
             if (data.password === admin.password) {
-                history.push("./admin");
+                history.push("./admin");  
             }
             else {
-                setErrorPassword('Password you entered is incorrect. Did you forget your password ?');
-            }
+            setErrorPassword('Password you entered is incorrect. Did you forget your password ?');   
+            }  
         }
-        else {
-            setErrorEmail('Email you entered does not match any account.')
+      
+       else{
+           
+        setErrorEmail('Email you entered does not match any account.')   
         }
+        
     }
     const history = useHistory();
     const admin = {
         email: "admin@gmail.com",
         password: "admin1234"
     }
-    return (
+    return (  
         <div className="div_">
+        <img src={Logo} className="logo" alt="Facebook logo"/>
             <div className="sign">
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <input className="signIn"
@@ -52,7 +56,7 @@ export const Sign_in = () => {
                         ref={register}
                         required
                     /><div>
-                        <span>{(errorEmail !== "") ? (<span className="err">{errorEmail}</span>) : ""}</span>
+                        <span>{(errorEmail !== "") ? (<span className="err">{errorEmail}</span>) :"" }</span>
                     </div>
                     <input className="signIn"
                         style={{ border: (errorPassword !== "") ? '1px solid red' : "" }}
@@ -63,18 +67,17 @@ export const Sign_in = () => {
                         required
                     />
                     {(errorPassword !== "") ? (<span className="err">{errorPassword}</span>) : ""}
-
                     <div>
                         <div className="div_btn">
                             <button className="btn" type="submit">Log In</button>
                         </div> </div>
                     <span className="psw"><a href="#">Forgot password ?</a></span>
                     <hr className="line"></hr>
-                    <Link to="/Sign_up"><button className="sign_up_btn">Sign Up</button></Link>
-
+                    <Link to="/Sign_up"><button className="sign_up_btn" >Sign Up</button></Link>
                 </form>
             </div>
         </div>
-    );
+         
+     );
 };
 
